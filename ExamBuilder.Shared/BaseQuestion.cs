6f336ac2b2e7c09fa6f3801e49971dff7ff0d0c6
amiralivali <amiralivali123123@gmail.com>
2026-02-28 00:29:34 +0330

    .

--- ExamBuilder.Shared/BaseQuestion.cs
+++ ExamBuilder.Shared/BaseQuestion.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ExamBuilder.Shared
 {
@@ -13,7 +8,7 @@ namespace ExamBuilder.Shared
         public int ID { get; set; }
         public int LessonID { get; set; }
         [Required]
-        [MaxLength(100)]
+        [MaxLength(200)]
         public string QuestionText { get; set; }
         public string Picture { get; set; }
         public int DifficultyLevelID { get; set; }
